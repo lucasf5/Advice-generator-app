def4397bce3af2b5d9f2261c504d548567a9a3ac
Lucas Ferreira <lucasfpnt@gmail.com>
2022-03-06 15:55:27 -0300

    enviando arquivos

---
+++ ts/js.ts
@@ -0,0 +1,16 @@
+const nota = document.querySelector(".nota")
+const botao = document.querySelector(".botao")
+const numeroAdvice = document.querySelector(".numeroAdvice")
+
+
+botao.addEventListener("click", () => {    
+    let random = Math.floor(Math.random() * 200);
+    const url = fetch(`https://api.adviceslip.com/advice/${random}`)
+    url.then((resposta) => {
+        return resposta.json();
+    }).then((dados) => {
+        nota.innerHTML = `"${dados.slip.advice}"`;
+        numeroAdvice.innerHTML = `ADVICE #${dados.slip.id}`;
+    })
+})
+
